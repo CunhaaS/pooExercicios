bab283d8257fa0a78359b913ca1483359f85481d
José Cunha <zezecunha4@gmail.com>
2018-03-15 11:55:31 +0000

    mais exercicios resolvidos

--- Ficha 4/script.js
+++ Ficha 4/script.js
@@ -10,17 +10,100 @@ function funcA(){
         course: "POO",
         grade: 12
     }
-    let listagem = listagem(student)
-    let tamanho = tamanho(student)
-    
-    show("A ->" + listagem)
-    show("A ->" + tamanho)
+    let listage = listagem(student)
+    let tamanh = tamanho(student)
+    let remove = remover(student)
+    show("A.a -> " + listage)
+    show("A.b -> " + remove)
+    show("A.c -> " + tamanh)
 }
 function listagem(student){
-    console.log(Object.keys(student))
-    return (Object.keys(student))
+    let txt = ""
+    for (let x in student) 
+    {
+        txt = txt + "nome: " + x + "\n"
+    }
+    return(txt)
+}
+function remover(student) {
+    let txt = ""
+    for (let x in student) 
+    {
+        txt += " valor: " + student[x] + "\n"
+    }
+    txt += "Removendo o grade \n"
+    delete student.grade
+    for (let x in student) 
+    {
+        txt += " valor: " + student[x] + "\n"
+    }
+    return(txt)
 }
 function tamanho(student){
-    console.log(Object.keys(student).length)
-    return (Object.keys(student).length)
+    let txt = 0
+    for (let x in student) 
+    {
+        txt++
+    }
+    return(txt)
+}
+// Alinea B
+function funcB(){
+    let grades = [
+        {
+        name: 'Rui',
+        grade: 8
+        },
+        {
+        name: 'Ana',
+        grade: 12
+        },
+        {
+        name: 'Carla',
+        grade: 14
+        }
+    ]
+    let inserido = inserir(grades)
+    let media = calculo(grades)
+    let positivo = notas(grades)
+    show("B.a -> " + inserido)
+    show("B.b -> " + media)
+    show("B.c -> " + positivo)
+}
+function inserir(grades){
+    
+    let txt = ""
+    let nome = prompt("Insira o seu Nome!")
+    let valor = prompt("Insira a sua Nota!")
+    for (let x in grades) 
+    {
+        txt += "nome: " + grades[x].name + " valor: " + grades[x].grade + "\n"
+    }
+    return(txt)
+}
+function calculo(grades){
+    
+    let soma = 0
+    let contadora = 0
+    let media = 0
+    for (let x in grades) 
+    {
+        soma += grades[x].grade
+        contadora++
+    }
+    media = soma / contadora
+    return(parseFloat(media.toFixed(2)) + "\n")
+}
+function notas(grades){
+    
+    let txt = ""
+    for (let x in grades) 
+    {
+        if(grades[x].grade >= 9.5)
+        {
+            txt += "nome: " + grades[x].name + " valor: " + grades[x].grade + "\n"
+        }
+        
+    }
+    return(txt)
 }
