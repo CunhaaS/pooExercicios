ee98f6102ebdb542bb4377abbace23732789aed5
José Cunha <zezecunha4@gmail.com>
2018-03-15 12:31:48 +0000

    ficha 3 resolvida, duvidas no exercicio D

--- Ficha 3/script.js
+++ Ficha 3/script.js
@@ -57,6 +57,33 @@ function d() {
     }
     console.log(contadora)
 }
+function e() {
+    let frase = prompt("Escolha a frase")
+    let fraseInvertida = frase.split("").reverse().join("")
+    console.log(fraseInvertida)
+}
+function f() {
+    let frase = prompt("Escolha a frase")
+    let caracter = prompt("Escolha o caracter")
+    let frase2 = frase
+    for (let i = 0; i < frase.length; i++) {
+        if(frase.charAt(i) == caracter)
+        {
+            frase2 = frase2.replace(caracter,"")
+        }
+    }
+    console.log(frase2)
+}
+function g() {
+    let segundos = parseInt(prompt("Digite os segundos"))
+    let segundos2 = segundos
+    let horas = parseInt(segundos / 3600)
+    segundos = segundos - (horas*3600)
+    let min = parseInt(segundos/60)
+    segundos = segundos - (min*60)
+    let txt = segundos2 + "s = "+horas+"h e "+ min +"min e "+ segundos +"s"
+    console.log(txt)
+}
 function h() {
     let frase = prompt("Escolha a frase")
     let abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
