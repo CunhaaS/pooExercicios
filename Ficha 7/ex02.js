069b28732fc3d06b6db3fca0312a6d85f5e1b38a
José Cunha <zezecunha4@gmail.com>
2018-04-05 14:49:37 +0100

    Ex02 a resolver

--- Ficha 7/ex02.js
+++ Ficha 7/ex02.js
@@ -2,5 +2,22 @@ window.onload = function () {
     init()
 }
 function init() {
-    
+    let email = document.getElementById("Email").value
+    let nome = document.getElementById("Nome").value
+    let tel = document.getElementById("tel").value
+    let user = document.getElementById("user").value
+    let pass1 = document.getElementById("Pass").value
+    let pass2 = document.getElementById("Pass2").value
+    let login = document.getElementById("Login") 
+
+    login.addEventListener("submit", function() {
+        if(pass1 != pass2)
+        {
+            alert("Palavra-passe diferente")
+        }
+        else{
+            alert("Registado")
+        }
+
+    })
 }
