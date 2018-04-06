c53762b8f5189dc19939e4bb562e2bc7cdbe6123
José Cunha <zezecunha4@gmail.com>
2018-04-06 10:20:57 +0100

    Ex02 resolvido

--- Ficha 7/ex02.js
+++ Ficha 7/ex02.js
@@ -5,19 +5,44 @@ function init() {
     let email = document.getElementById("Email").value
     let nome = document.getElementById("Nome").value
     let tel = document.getElementById("tel").value
+    let data = document.getElementById("date").value
     let user = document.getElementById("user").value
     let pass1 = document.getElementById("Pass").value
     let pass2 = document.getElementById("Pass2").value
     let login = document.getElementById("Login") 
 
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; 
+    let yyyy = today.getFullYear();
+    let dia = mm + '/' + dd + '/' + yyyy
     login.addEventListener("submit", function() {
-        if(pass1 != pass2)
+        
+        if(data != null){
+            if(data > dia){
+                if(pass1 != pass2)
+                {
+                    alert("Palavra-passe diferente")
+                }
+                else{
+                    alert("Registado")
+                }
+            }
+            else{
+                alert("Data inválida")
+            }
+        }
+        else
         {
-            alert("Palavra-passe diferente")
+            if(pass1 != pass2)
+                {
+                    alert("Palavra-passe diferente")
+                }
+                else{
+                    alert("Registado")
+                }
         }
-        else{
-            alert("Registado")
-        }
+        
 
     })
 }
